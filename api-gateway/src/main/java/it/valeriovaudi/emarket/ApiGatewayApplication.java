3806d4fb066c45178b42ca29c9f450398226a2cf
mrFlick72 <valerio.vaudi@gmail.com>
2019-01-30 22:19:07 +0100

    new oauth2 resourceserver configuration

--- api-gateway/src/main/java/it/valeriovaudi/emarket/ApiGatewayApplication.java
+++ api-gateway/src/main/java/it/valeriovaudi/emarket/ApiGatewayApplication.java
@@ -3,10 +3,8 @@ package it.valeriovaudi.emarket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableHystrix
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
